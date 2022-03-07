3529a96bca99715401e3d602b353cc76a82a512a
cedliang <clia0017@student.monash.edu>
2022-03-08 03:29:18 +1100

    formatted

--- Labs/week2/q1.py
+++ Labs/week2/q1.py
@@ -1,19 +1,18 @@
 import math
 import functools
 
-# composeBin
-# powers_list is of the form
-# [[exponent of two, 1 or 0]]
+
 def composeBin(powers_list: list[list[int, int]]):
     if (returnstr := functools.reduce(lambda prevString, digit_pair: prevString+str(digit_pair[1])+"." if digit_pair[0] == 0
                                       else prevString+str(digit_pair[1]), powers_list, ""))[-1] == ".":
         return returnstr[:-1]
     if (appendZeros := powers_list[-1][0]) > 0:
-        return returnstr+appendZeros*"0"   
+        return returnstr+appendZeros*"0"
     return returnstr
 
-def toBinAux(currentexp: int, currentn: int, current_powerslist: list[list[int,int]]):
-    #base case
+
+def toBinAux(currentexp: int, currentn: int, current_powerslist: list[list[int, int]]):
+    # base case
     if currentexp == -16 or currentn == 0:
         return currentexp, currentn, current_powerslist
     else:
@@ -21,8 +20,10 @@ def toBinAux(currentexp: int, currentn: int, current_powerslist: list[list[int,i
             return toBinAux(currentexp-1, currentn - 2**currentexp, current_powerslist+[[currentexp, 1]])
         return toBinAux(currentexp-1, currentn, current_powerslist+[[currentexp, 0]])
 
+
 def toBin(real_n: float):
     return composeBin(toBinAux(math.floor(math.log(real_n, 2)), real_n, [])[2])
+
 
 if __name__ == "__main__":
     dec_num: float = 88.893
