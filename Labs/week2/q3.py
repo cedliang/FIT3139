d26c4e576e9a08673b30ed39811cbb4ad7e0d09f
cedliang <clia0017@student.monash.edu>
2022-03-13 14:26:46 +1100

    modified to use generator

--- Labs/week2/q3.py
+++ Labs/week2/q3.py
@@ -1,24 +1,26 @@
 import math
+import itertools
 
-from nbformat import current_nbformat
-
-# n \in 0 <= n <= inf
-
-
+#curried approach
 def e_x_nthterm(x: int):
     def f(n: int):
         return (x**n)/math.factorial(n)
     return f
 
+#generator approach
+def e_x_nthterm_gen(x: int):
+    n = 0
+    while True:
+        yield (x**n)/math.factorial(n)
+        n += 1
+
 
 def fixed_num_terms(x: int, num_terms: int):
     current_approx = 0
 
-    # note that range() initiates the n value for the series at 0.
-    # A series that begins at 1 will require a different range term
-    for n in range(num_terms):
+    for nth_term in itertools.islice(e_x_nthterm_gen(x), num_terms):
         last_approx = current_approx
-        current_approx += e_x_nthterm(x)(n)
+        current_approx += nth_term
         approx_error = (current_approx - last_approx)/current_approx
 
         print(approx_error)
@@ -29,13 +31,13 @@ def fixed_num_terms(x: int, num_terms: int):
 # tolerance_exponent should be a negative integer for best results, ie -12
 def error_tolerance(x: int, tolerance_exponent: int):
     current_approx = 0
-
+    series_term_generator = e_x_nthterm_gen(x)
     n = 0
     firstLoop = True
 
     while firstLoop or approx_error > 10**tolerance_exponent:
         last_approx = current_approx
-        current_approx += e_x_nthterm(x)(n)
+        current_approx += next(series_term_generator)
         approx_error = (current_approx - last_approx)/current_approx
 
         if firstLoop:
@@ -48,6 +50,6 @@ def error_tolerance(x: int, tolerance_exponent: int):
 
 
 if __name__ == "__main__":
-    # fixed_num_terms(10,40)
+    #fixed_num_terms(10,40)
 
     error_tolerance(10, -16)
