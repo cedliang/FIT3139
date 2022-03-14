9aee8a3e8d9115bd22c72088c0ca138c643abeb6
cedliang <clia0017@student.monash.edu>
2022-03-14 22:37:13 +1100

    q3 forward elimination implemented

--- Labs/week3/q3.py
+++ Labs/week3/q3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import lu
 
 
 def back_substitution(u: np.ndarray, z: np.ndarray):
@@ -14,9 +15,41 @@ def back_substitution(u: np.ndarray, z: np.ndarray):
     return _back_substitution(len(z))([None]*len(z), len(z)-1)
 
 
+def forward_elimination(a: np.ndarray, b: np.ndarray):
+    def generate_elim_matrix(a: np.ndarray, n: int):
+        def column_value(pivot_value):
+            def g(current_index, current_value):
+                if current_index < n:
+                    return 0
+                elif current_index == n:
+                    return 1
+                else:
+                    return -1*(current_value/pivot_value)
+            return g
+
+        elim_matrix_nth_column = np.array(list(map(column_value(
+            (source_column := a[:, n])[n]), range(len(source_column)), source_column)))
+        ident = np.identity(len(a[0]))
+        ident[:, n] = elim_matrix_nth_column
+        return ident
+
+    def _forward_elimination(a: np.ndarray, b: np.ndarray, n: int):
+        e_mat = generate_elim_matrix(a, n)
+        return (np.dot(e_mat, a), np.dot(e_mat, b)) if n != len(b) - 1 else _forward_elimination(np.dot(e_mat, a), np.dot(e_mat, b), n+1)
+
+    return _forward_elimination(a, b, 0)
+
+
 if __name__ == "__main__":
     upper_triangular = np.array([[1, 2, 2], [0, -4, -6], [0, 0, -1]])
     z = np.array([3, -6, -2])
 
     print("Np answer:\n", np.linalg.solve(upper_triangular, z))
     print("My answer:\n", back_substitution(upper_triangular, z))
+
+    print("Forward elimination:")
+    a = np.array([[1, 2, 2], [4, 4, 2], [4, 6, 4]])
+    b = np.array([3, 6, 10])
+    u, z = forward_elimination(a, b)
+    print("u\n", u)
+    print("z\n", z)
