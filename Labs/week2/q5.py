336810fa37053a7dfacfce359a389c0c74555d84
Cedric Liang <62864170+cedliang@users.noreply.github.com>
2022-03-17 19:04:01 +1100

    Update q5.py

--- Labs/week2/q5.py
+++ Labs/week2/q5.py
@@ -1,13 +1,12 @@
-
 import sys
 
 
 if __name__ == "__main__":
     e = 1.0
-    while e > 0:
+    while e + 1 > 1:
         if (e/2) + 1 > 1:
-             e = e/2
-         else:
-             break
+            e = e/2
+        else:
+            break
 
     print(e, e == sys.float_info.epsilon)
