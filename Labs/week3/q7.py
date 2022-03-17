d929ad0aaf857afceb91cdf9d992fb80d6505ba9
cedliang <clia0017@student.monash.edu>
2022-03-17 14:54:30 +1100

    week 3 added solve using palu decomp to q7

--- Labs/week3/q7.py
+++ Labs/week3/q7.py
@@ -1,21 +1,33 @@
 from q6 import forward_elimination_with_swapping
 from q4 import right_to_left_dotprod
+from q3 import back_substitution
 import numpy as np
 import itertools
 import functools
 
 
+def solve_by_lu_fac(a, b):
+    p, l, u = pa_lu_factorise(a, b)
+
+    pb = np.dot(p, b)
+    ys = np.dot(np.linalg.inv(l), pb)
+
+    return back_substitution(u, ys)
+
+
 def pa_lu_factorise(a: np.ndarray, b: np.ndarray):
     u, z, e_mats, ps = forward_elimination_with_swapping(a, b)
 
-    bad_order_l = np.linalg.inv(np.array(functools.reduce(right_to_left_dotprod, itertools.chain.from_iterable(map(list, (zip(ps, e_mats)))))))
+    bad_order_l = np.linalg.inv(np.array(functools.reduce(
+        right_to_left_dotprod, itertools.chain.from_iterable(map(list, (zip(ps, e_mats)))))))
     combined_p = np.array(functools.reduce(right_to_left_dotprod, ps))
     l = np.dot(combined_p, bad_order_l)
 
     return combined_p, l, u
 
+
 if __name__ == "__main__":
-    a = np.array([[1, 2, 2], [4, 4, 2], [4, 6, 4]])
+    a = np.array([[5, 2, 7], [7, 3, 7], [1, 32, 8]])
     b = np.array([3, 6, 10])
 
     p, l, u = pa_lu_factorise(a, b)
@@ -24,3 +36,5 @@ if __name__ == "__main__":
     print("a\n", a)
     print("l\n", l)
     print("u\n", u)
+
+    print(solve_by_lu_fac(a, b))
