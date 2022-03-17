229ff5cd11f1cdf68d2227110e31520de4caef45
Cedric Liang <62864170+cedliang@users.noreply.github.com>
2022-03-17 19:04:31 +1100

    Update q6.py

--- Labs/week2/q6.py
+++ Labs/week2/q6.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     e = 1.0
     while e > 0:
         if (e/2) > 0:
-             e = e/2
-         else:
-             break
+            e = e/2
+        else:
+            break
+
     print(e, e == sys.float_info.min*sys.float_info.epsilon)
