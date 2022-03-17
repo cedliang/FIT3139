64e70f1c4654bf726a307bbb369965b9d9193f85
Sourcery AI <>
2022-03-17 07:55:01 +0000

    'Refactored by Sourcery'

--- Labs/week2/q5.py
+++ Labs/week2/q5.py
@@ -4,10 +4,6 @@ import sys
 
 if __name__ == "__main__":
     e = 1.0
-    while e + 1 > 1:
-        if (e/2) + 1 > 1:
-            e = e/2
-        else:
-            break
-
+    while e > 0:
+        e /= 2
     print(e, e == sys.float_info.epsilon)
